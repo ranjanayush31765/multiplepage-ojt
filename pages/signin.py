b5c814772a3ae1c0bb25883189fa265a36571c70
ranjanayush31765 <ranjanayush31765@gmail.com>
2026-05-12 23:19:49 +0530

    Add files via upload

---
+++ pages/signin.py
@@ -0,0 +1,20 @@
+import streamlit as st
+import pymongo
+
+
+
+st.title("SignIn")
+t1=st.text_input("Username")
+t2=st.text_input("Password")
+if st.button("SIGNIN"):
+       conn=pymongo.MongoClient("mongodb://127.0.0.1:27017/?directConnection=true&serverSelectionTimeoutMS=2000&appName=mongosh+2.8.2")
+       mydb=conn["cv"]
+       my=mydb["user_info"]
+       res=my.find({"username":t1,"password":t2})
+       v=0
+       for data in res:
+              v=v+1
+              st.switch_page("pages/profile.py")
+              
+       if v==0:
+              st.success("Invalid Login !!!")
