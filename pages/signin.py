5dc56446fe53baa797fe8c89afc8eb014e142777
ranjanayush31765 <ranjanayush31765@gmail.com>
2026-05-16 22:10:27 +0530

    Update signin.py

--- pages/signin.py
+++ pages/signin.py
@@ -7,7 +7,7 @@ st.title("SignIn")
 t1=st.text_input("Username")
 t2=st.text_input("Password")
 if st.button("SIGNIN"):
-       conn=pymongo.MongoClient("mongodb://127.0.0.1:27017/?directConnection=true&serverSelectionTimeoutMS=2000&appName=mongosh+2.8.2")
+       conn=pymongo.MongoClient("mongodb+srv://ranjanayush31765_db_user:EW9kHFHIlxV79Akj@cluster0.bnmawun.mongodb.net/?appName=Cluster0")
        mydb=conn["cv"]
        my=mydb["user_info"]
        res=my.find({"username":t1,"password":t2})
